298a78a92fe7b12a9c0b54df8e0be3e1fda4cecc
Timothy Gu <timothygu99@gmail.com>
2016-08-10 11:32:32 -0700

    Update deps; use promise

--- src/stop.js
+++ src/stop.js
@@ -1,6 +1,8 @@
 import {createServer} from 'http';
 import {parse as urlParse} from 'url';
 import {join} from 'path';
+import {denodeify} from 'promise';
+import rimraf from 'rimraf';
 import stop from 'stop';
 
 import app from './index.js';
@@ -8,25 +10,35 @@ import app from './index.js';
 process.env.NODE_ENV = 'production';
 
 const server = createServer(app);
-server.on('listening', () => {
-  const {port} = server.address();
-  console.log(`listening at http://localhost:${port}`);
+const output = join(__dirname, '..', 'output');
 
-  stop.getWebsiteStream(`http://localhost:${port}/en/api/reference.html`, {
+denodeify(rimraf)(output).then(() => {
+  return new Promise((resolve, reject) => {
+    server.listen(0);
+    server.on('error', reject);
+    server.on('listening', () => {
+      const {port} = server.address();
+
+      console.log(`listening at http://localhost:${port}`);
+      resolve(port);
+      server.removeListener('error', reject);
+    });
+  });
+}).then(port => {
+  return stop.getWebsiteStream(`http://localhost:${port}/en/api/reference.html`, {
     filter: currentURL => urlParse(currentURL).hostname === 'localhost',
     parallel: 1
   })
   .syphon(stop.minifyJS())
   .syphon(stop.minifyCSS())
   .syphon(stop.log())
-  .syphon(stop.checkStatusCodes([200, 404]))
-  .syphon(stop.writeFileSystem(join(__dirname, '..', 'output')))
-  .wait().done(() => {
-    console.log('success');
-    server.close(() => {
-      // HACK: for some reason, server.close() doesn't make the process exit
-      process.exit(0);
-    });
+  .syphon(stop.checkStatusCodes([200, /* FIXME */ 404]))
+  .syphon(stop.writeFileSystem(output))
+  .wait();
+}).then(() => {
+  console.log('success');
+  server.close(() => {
+    // HACK: for some reason, server.close() doesn't make the process exit
+    process.exit(0);
   });
 });
-server.listen(0);
