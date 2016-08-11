5416002914ac5298123ca5f14d86279888f99fcb
Timothy Gu <timothygu99@gmail.com>
2016-08-11 14:53:29 -0700

    Reorganize templates

--- src/browser/menu.js
+++ src/browser/menu.js
@@ -4,6 +4,5 @@ const menu = document.querySelector('.menu.outside');
 const toggle = menu.querySelector('.toggle')
 
 toggle.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
   menu.classList.toggle('show');
 });
