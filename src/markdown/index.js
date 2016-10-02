8c73ffcff753bffc354407cad88970e4b680ba0b
Lion Yang <lion@aosc.xyz>
2016-10-03 00:04:38 +0800

    Add specified-anchor syntax for multilingual, using `~~` (#12)
    * Add specified-anchor syntax for multilingual, using `~~`
    
    * Slightly condense the slugifying code
    
    * Fix headings with code blocks

--- src/markdown/index.js
+++ src/markdown/index.js
@@ -9,10 +9,30 @@ export const md = new MarkdownIt({
   typographer: true
 });
 
+const defaultSlugify = mdItAnchor.defaults.slugify;
 md.use(mdItAnchor, {
   level: 2,
+  slugify: string => {
+    const tail = /~~(.+)$/.exec(string);
+    return defaultSlugify(tail ? tail[1] : string);
+  },
   permalink: true
 });
+
+md.use((md, syntax) => {
+  md.core.ruler.push('heading_comments', ({tokens}) => {
+    for (let [i, token] of tokens.entries()) {
+      if (token.type === 'heading_open') {
+        const heading = tokens[i + 1];
+        // When heading level is greater than `<h1>`, in order to find the last
+        // text token we need to offset the anchor symbol.
+        const offset = token.tag === 'h1' ? 1 : 5;
+        const title = heading.children[heading.children.length - offset];
+        title.content = title.content.replace(syntax, '');
+      }
+    }
+  });
+}, /\s+~~(.+)$/);
 
 md.use(mdItCodeBlock);
 
