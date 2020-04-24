3dc0729550c9b10bfad7d1535283095ea331f9ac
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-24 12:10:20 +0100

    docs: add support pages

--- src/browser/menu.js
+++ src/browser/menu.js
@@ -9,10 +9,20 @@ toggle.addEventListener('click', () => {
 
 // Delay rendering carbon ads by 500ms to make sure they don't slow down page load
 setTimeout(() => {
+  // Render Carbon Ads
   const script = document.createElement('script');
   const container = document.querySelector('.menu.inside .advert');
   script.async = true;
   script.src = '//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=pugjsorg';
   script.id = '_carbonads_js';
   container.appendChild(script);
+
+  // Render e-mail list signup form
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//forms.aweber.com/form/06/692213006.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, "script", "aweber-wjs-fhbbgl26s"));
 }, 500);
